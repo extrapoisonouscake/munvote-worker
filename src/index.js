8138e63ad31c6a98ceac506e6855a58c8a7b4ebe
Vlad Gubarev <extrapoisonouscake@gmail.com>
2025-04-11 11:47:05 -0700

    a

--- src/index.js
+++ src/index.js
@@ -8,19 +8,26 @@ async function handleRequest(request) {
 
   // Replace the hostname with munvote.com
   url.hostname = "munvote.com";
-
+  const isGet = request.method === "GET";
   // Generate a random query parameter to ensure cache bypass
-  const randomQueryParam = `?nocache=${Math.random()
-    .toString(36)
-    .substring(2)}`;
 
   // Append the random query parameter to the URL
-  url.search = randomQueryParam;
+  if (isGet) {
+    const params = new URLSearchParams(url.search);
+
+    // Add the random 'nocache' query parameter to the URL
+    params.set("nocache", Math.random().toString(36).substring(2));
+
+    // Set the modified search parameters back to the URL
+    url.search = params.toString();
+  }
   console.log(url.toString());
   // Make the actual request to your origin (munvote.com)
   const response = await fetch(url.toString(), {
     method: request.method,
     headers: request.headers,
+    body: isGet || request.method === "HEAD" ? null : request.body,
+    redirect: "follow",
   });
 
   // Clone the response so we can modify headers
